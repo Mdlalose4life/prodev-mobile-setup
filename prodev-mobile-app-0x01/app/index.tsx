cf8928ea5187670d442ae7a57636cda3b79c04fe
Mdlalose4life <blessing1059@gmail.com>
2025-07-10 22:18:16 +0200

    ALX is awesome

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
       <Text style={styles.largeText}>Typescript is great if you practice more</Text>
       <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
